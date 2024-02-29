e03b98f05a0b6b38143ba777b5c4c79c171a733f
anishnairva <anishnairva@gmail.com>
2024-02-29 17:44:02 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "cnnClassifier"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+
+    if not os.path.exists(filepath):
+        os.makedirs(filepath.parent, exist_ok=True)
+        logging.info(f"Creating directory: {filepath.parent}")
+
+    if not os.path.exists(filepath) or os.path.getsize(filepath) == 0:
+        logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filepath.name} already exists")
